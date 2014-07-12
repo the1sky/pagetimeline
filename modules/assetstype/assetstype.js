68e66408d6f0c391ff6f1116889b85126e1ac768
the1sky <dumingtan@gmail.com>
2014-07-13 03:55:08 +0800

    # chrome assets type support, but not contain "Tansfer-Encoding:chunked"

---
+++ modules/assetstype/assetstype.js
@@ -0,0 +1,63 @@
+/**
+ * Created by nant on 2014/7/13.
+ */
+
+exports.version = '0.1';
+
+exports.module = function(pagetimeline,callback){
+	var requests = pagetimeline.core.requests;
+	var _ = require('underscore');
+	var mime  = require('mime');
+	var assertsInfo = {}
+
+	mime.define({
+		'text/javascript': ['js'],
+		'application/x-javascript':['js']
+	});
+
+	_.each(requests,function(value,key){
+		var url = value.url;
+		var responseBody = value.responseBody;
+		var mimeExt = mime.extension( responseBody.mimeType );
+		/*
+		if( !mimeExt ){
+			console.log( mimeExt );
+		}
+		if( mimeExt == 'js' ){
+			console.log(mimeExt );
+		}*/
+		var contentLen = 0;
+		if( responseBody.headers['Content-Length'] ){
+			contentLen = parseFloat( responseBody.headers['Content-Length'] );
+		}else{
+			contentLen = 0;
+			/*
+			var browser = pagetimeline.core.browser;
+			browser.send('Network.getResponseBody',{requestId:key},function(err,data){
+				"use strict";
+				console.log(data);
+			})
+			*/
+		}
+		if( !assertsInfo[mimeExt] ){
+			assertsInfo[mimeExt] = {
+				count:0,
+				size:0,
+				urls:[]
+			}
+		}
+
+		assertsInfo[mimeExt].count ++;
+		assertsInfo[mimeExt].size += contentLen;
+		assertsInfo[mimeExt].urls.push( url );
+	});
+
+	_.each(assertsInfo,function(value,key){
+		pagetimeline.setMetric(key, '   count:' + value.count + '  size:' + ( value.size / 1024 ).toFixed(2) + 'KB'  );
+		_.each( value.urls,function(url){
+			pagetimeline.addOffender(key, url );
+		})
+	})
+
+	callback(false,' get assets type succ!');
+}
