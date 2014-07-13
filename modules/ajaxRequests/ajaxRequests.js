29cd9ed65e441d5e7757213d90d791ee9f7305d5
the1sky <dumingtan@gmail.com>
2014-07-13 23:36:27 +0800

    # support 'ajax requests', not validated

---
+++ modules/ajaxRequests/ajaxRequests.js
@@ -0,0 +1,46 @@
+/**
+ * Created by nant on 2014/7/13.
+ */
+exports.version = '0.1';
+
+exports.module = function(pagetimeline,callback){
+	var start = +new Date()
+	pagetimeline.log( 'ajax requests...' );
+	var requests = pagetimeline.core.requests;
+	var _ = require('underscore');
+	var keyName = 'ajaxRequests';
+	var assertsInfo = {
+		count:0,
+		size:0,
+		urls:[]
+	}
+
+	_.each(requests,function(value,key){
+		var url = value.url;
+		var responseBody = value.responseBody;
+		var requestHeaders = responseBody.requestHeaders;
+
+		if( requestHeaders && ( requestHeaders[ 'x-requested-with'] == 'XMLHttpRequest' ) ){
+			var contentLen = 0;
+			if( responseBody.headers['Content-Length'] ){
+				contentLen = parseFloat( responseBody.headers['Content-Length'] );
+			}else{
+				contentLen = 0;
+			}
+
+			assertsInfo.count ++;
+			assertsInfo.size += contentLen;
+			assertsInfo.urls.push( url );
+		}
+	});
+
+	if( assertsInfo.count ){
+		pagetimeline.setMetric(keyName, '   count:' + assertsInfo.count + '  size:' + ( assertsInfo.size / 1024 ).toFixed(2) + 'KB'  );
+		_.each( assertsInfo.urls,function(url){
+			pagetimeline.addOffender(keyName, url );
+		})
+	}
+
+	pagetimeline.log( 'ajax requests done in ' + (+new Date() - start ) + 'ms' );
+	callback(false,' get ajax requests succ!');
+}
