4c694bc79b98118b2ba9d33f299978c92cd29c51
the1sky <dumingtan@gmail.com>
2014-07-14 05:14:33 +0800

    # fixed

--- modules/ajaxRequests/ajaxRequests.js
+++ modules/ajaxRequests/ajaxRequests.js
@@ -18,7 +18,7 @@ exports.module = function(pagetimeline,callback){
 	_.each(requests,function(value,key){
 		var url = value.url;
 		var responseBody = value.responseBody;
-		var requestHeaders = responseBody.requestHeaders;
+		var requestHeaders = responseBody['requestHeaders'];
 
 		if( requestHeaders && ( requestHeaders[ 'X-Requested-With'] == 'XMLHttpRequest' ) ){
 			var contentLen = 0;
