941a79cf2a398abf2ec39ca1940d483b6f9d3b97
the1sky <dumingtan@gmail.com>
2014-08-04 20:07:40 +0800

    # add module, redirects

---
+++ modules/redirects/redirects.js
@@ -0,0 +1,25 @@
+/**
+ * Created by nant on 2014/8/4.
+ */
+
+exports.version = '0.1';
+
+exports.run = function(pagetimeline,callback){
+	callback( false, {message:'add redirects module done!'});
+
+	var browser = pagetimeline.model.browser;
+
+	browser.onResponseReceived(function(res){
+		var status = res.response.status;
+		if( status >= 300 && status < 400 && status !=304 ){
+			var statusText = res.response.statusText;
+			var url = res.response.url;
+			var redirectUrl = res.response.redirectURL;
+			var location = res.response.headers.Location;
+
+			pagetimeline.incrMetric('redirects');
+			pagetimeline.addOffender('redirects', url + ' is a redirect (HTTP ' + status + ' ' + statusText + ') ' +
+				'to ' + ( redirectUrl || location ) );
+		}
+	});
+}
