76f49676df21d31e8e5914c87b85835e16f4b17c
the1sky <dumingtan@gmail.com>
2014-08-04 17:13:01 +0800

    # add module, documentHeight

---
+++ modules/documentHeight/documentHeight.js
@@ -0,0 +1,28 @@
+/**
+ * Created by nant on 2014/8/4.
+ */
+
+exports.name = 'documentHeight';
+exports.version = '0.1';
+
+exports.run = function(pagetimeline,callback){
+	callback( false, {message:'add document height module done!'});
+	var browser = pagetimeline.model.browser;
+	var timeout = pagetimeline.getParam('timeout');
+
+	browser.onLoadEventFired(function(res){
+		setTimeout(function(){
+			var script = getDocumentHeight.toString() + ';getDocumentHeight()';
+			browser.evaluate( script, function(err,res){
+				if( !err ){
+					pagetimeline.setMetric('documentHeight', res.result.value );
+				}
+			})
+		}, timeout);
+	})
+
+	function getDocumentHeight(){
+		var doc = document, body = doc.body, docelem = doc.documentElement;
+		return Math.max( body.scrollHeight, body.offsetHeight, docelem.clientHeight, docelem.scrollHeight, docelem.offsetHeight );
+	}
+}
