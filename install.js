f090616abfab4f9842cb1ea39c4bc2d7f3127de1
the1sky <dumingtan@gmail.com>
2014-08-08 10:56:47 +0800

    # ubuntu, xvfb and chrome install shell

--- install.js
+++ install.js
@@ -25,7 +25,12 @@ var originalPath = process.env.PATH
 var tmpPath = null;
 
 if( process.platform == 'linux' ){
-	//install browser
+	var BrowserScript = require('./libs/browserScript');
+	var bs = new BrowserScript({browser:'chrome'});
+	bs.installBrowser(function(err,res){
+		console.log(err,res);
+		exit( 0 );
+	});
 }else{
 	//download portable browser
 	var downloadUrl = 'http://fe.baidu.com/pagetimeline/tools/ChromiumPortable.zip';
