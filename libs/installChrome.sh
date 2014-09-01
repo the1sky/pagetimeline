70ac26080efeb8728d0080aa227c29730183fcfe
the1sky <dumingtan@gmail.com>
2014-09-01 22:50:30 +0800

    # install, support centos

--- libs/installChrome.sh
+++ libs/installChrome.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+cat /etc/issue | grep CentOS
+if [ $? -eq 0 ]; then
+    wget http://chrome.richardlloyd.org.uk/install_chrome.sh
+    chmod +x install_chrome.sh
+    sh install_chrome.sh
+fi
+
+
+cat /etc/issue | grep Ubuntu
+if [ $? -eq 0 ]; then
+    sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt-get update -y
+    sudo apt-get install -y google-chrome-stable
+fi
--- libs/installXvfb.sh
+++ libs/installXvfb.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install -y xvfb fluxbox x11vnc dbus libasound2 libqt4-dbus libqt4-network libqtcore4 libqtgui4 libxss1 libpython2.7 libqt4-xml libaudio2 libmng1 fontconfig liblcms1 lib32stdc++6 lib32asound2 ia32-libs libc6-i386 lib32gcc1 nano
-sudo apt-get install -y python-gobject-2
-sudo apt-get install -y curl git
+cat /etc/issue | grep CentOS
+if [ $? -eq 0 ]; then
+    yum -y install xorg-x11-server-Xvfb
+fi
+
+cat /etc/issue | grep Ubuntu
+if [ $? -eq 0 ]; then
+    sudo apt-get update
+    sudo apt-get install -y xvfb fluxbox x11vnc dbus libasound2 libqt4-dbus libqt4-network libqtcore4 libqtgui4 libxss1 libpython2.7 libqt4-xml libaudio2 libmng1 fontconfig liblcms1 lib32stdc++6 lib32asound2 ia32-libs libc6-i386 lib32gcc1 nano
+    sudo apt-get install -y python-gobject-2
+    sudo apt-get install -y curl git
+fi
 
